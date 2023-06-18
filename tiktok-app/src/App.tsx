36dd6c5f10aaab1ecd1cb4d7add35c4e41e6d969
anthao5032 <136871946+anthao5032@users.noreply.github.com>
2023-06-18 21:22:30 +0700

    18/6/23 9:21pm create user detail

--- tiktok-app/src/App.tsx
+++ tiktok-app/src/App.tsx
@@ -1,13 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import Root from './pages/Root/Root';
-import NewsFeed from './pages/User/NewsFeed/NewsFeed';
-import ComingSoon from './components/ComingSoon';
+import { Route, Routes } from "react-router-dom";
+import Root from "./pages/Root/Root";
+import NewsFeed from "./pages/User/NewsFeed/NewsFeed";
+import ComingSoon from "./components/ComingSoon";
+import UserDetail from "./components/UserDetail";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Root />}>
-        <Route path="/" element={<NewsFeed />} />
+        <Route path="/" element={<NewsFeed />}></Route>
+        <Route path="/userId" element={<UserDetail />}></Route>
         <Route path="/following" element={<NewsFeed />} />
         <Route path="/explore" element={<ComingSoon />}></Route>
         <Route path="/live" element={<ComingSoon />}></Route>
