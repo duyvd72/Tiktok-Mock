7c716155af524a2ce985d6106cc98ab5bce2111b
anthao5032 <136871946+anthao5032@users.noreply.github.com>
2023-06-30 00:47:40 +0700

    30/06/23 12:46PM update userdetail

--- tiktok-app/src/components/EditUserInfor.tsx
+++ tiktok-app/src/components/EditUserInfor.tsx
@@ -1,13 +1,13 @@
-import styles from '@/styles/userDetail.module.css';
-import axios from 'axios';
-import { ChangeEvent, useContext, useState } from 'react';
-import { ErrorMessage, Field, Formik } from 'formik';
-import * as Yup from 'yup';
-import { InitialValuesContext } from './UserDetail';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useParams } from 'react-router-dom';
-import defaultAva from '@/assets/images/default-ava.png';
+import styles from "@/styles/userDetail.module.css";
+import axios from "axios";
+import { ChangeEvent, useContext, useState } from "react";
+import { ErrorMessage, Field, Formik } from "formik";
+import * as Yup from "yup";
+import { InitialValuesContext } from "./UserDetail";
+import { useParams } from "react-router-dom";
+import defaultAva from "@/assets/images/default-ava.png";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { storage } from "@/firebase/index";
 
 interface EditUserInforProps {
   setClickEdit: React.Dispatch<React.SetStateAction<boolean>>;
@@ -20,23 +20,39 @@ const EditUserInfor = ({
 }: EditUserInforProps) => {
   const initialValues = useContext(InitialValuesContext);
   const [changeImage, setChangeImage] = useState(false);
-  const [imageState, setImageState] = useState('');
+  const [imageState, setImageState] = useState("");
   const { userId } = useParams();
-  console.log(imageState);
-
+  const [currentFile, setCurrentFile] = useState<any>();
   const handleUpdateUser = (values: any) => {
-    axios
-      .put(
-        `${import.meta.env.VITE_BACKEND_URL}/accounts/${userId}/update`,
-        values
-      )
-      .then((res) => {
-        toast.success('Cập nhật thành công');
-        setChangeImage(false);
-        setClickEdit(false);
-        setCurrentUser(res.data.user);
-      })
-      .catch((err) => console.error(err));
+    const storageRef = ref(storage, currentFile[0].name);
+    const uploadTask = uploadBytesResumable(storageRef, currentFile[0]);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.error(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((imageUrl) => {
+          axios
+            .put(
+              `${import.meta.env.VITE_BACKEND_URL}/accounts/${userId}/update`,
+              {
+                ...values,
+                avatarUrl: imageUrl,
+              }
+            )
+            .then((res) => {
+              setChangeImage(false);
+              setClickEdit(false);
+              setCurrentUser(res.data.user);
+              setImageState("");
+            })
+            .catch((err) => console.error(err));
+        });
+      }
+    );
   };
 
   const handleImage = (e: ChangeEvent<HTMLInputElement>) => {
@@ -44,6 +60,7 @@ const EditUserInfor = ({
     if (file) {
       setImageState(URL.createObjectURL(file));
       setChangeImage(true);
+      setCurrentFile(e.target.files);
     }
   };
 
@@ -53,12 +70,12 @@ const EditUserInfor = ({
         initialValues={initialValues}
         validationSchema={Yup.object().shape({
           nickname: Yup.string()
-            .required('Không được để trống TikTokID')
-            // .matches(/^[A-Za-z0-9_.]+$/, "Đoạn mã không hợp lệ")
-            .min(3, 'TikTokID phải gồm ít nhất 3 kí tự')
-            .max(24, 'Tối đa 24 kí tự'),
-          fullname: Yup.string().max(30, 'Tối đa 30 kí tự'),
-          bio: Yup.string().max(80, 'Tối đa 80 kí tự'),
+            .required("Không được để trống TikTokID")
+            .matches(/^[A-Za-z0-9_.]+$/, "Đoạn mã không hợp lệ")
+            .min(3, "TikTokID phải gồm ít nhất 3 kí tự")
+            .max(24, "Tối đa 24 kí tự"),
+          fullname: Yup.string().max(30, "Tối đa 30 kí tự"),
+          bio: Yup.string().max(80, "Tối đa 80 kí tự"),
         })}
         onSubmit={(values) => {
           handleUpdateUser(values);
@@ -85,26 +102,42 @@ const EditUserInfor = ({
                   <p className="w-1/4 font-bold">Ảnh hồ sơ</p>
                   <div className="w-2/4 relative">
                     <div className="w-fit m-auto cursor-pointer">
-                      <img
-                        src={
-                          initialValues.avatarUrl
-                            ? initialValues.avatarUrl
-                            : defaultAva
-                        }
-                        alt="avatar"
-                        className="w-28 h-28 rounded-full cursor-pointer m-auto"
-                      />
+                      {imageState === "" ? (
+                        <label htmlFor="img" className="cursor-pointer">
+                          <img
+                            src={
+                              initialValues.avatarUrl
+                                ? initialValues.avatarUrl
+                                : defaultAva
+                            }
+                            alt="avatar"
+                            className="w-28 h-28 rounded-full cursor-pointer m-auto"
+                          />
+                        </label>
+                      ) : (
+                        <label htmlFor="img" className="cursor-pointer">
+                          <img
+                            src={imageState}
+                            alt="avatar"
+                            className="w-28 h-28 rounded-full cursor-pointer m-auto"
+                          />
+                        </label>
+                      )}
+
                       <div className="rounded-full border border-gray bg-white absolute p-2 left-1/2 translate-x-2/4 bottom-0">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          height="20"
-                          viewBox="0 0 512 512"
-                          fill="#000000"
-                        >
-                          <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" />
-                        </svg>
+                        <label htmlFor="img" className="cursor-pointer">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="20"
+                            viewBox="0 0 512 512"
+                            fill="#000000"
+                          >
+                            <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" />
+                          </svg>
+                        </label>
                         <input
                           type="file"
+                          id="img"
                           accept=".jpg,.jpeg,.png,.tiff,.heic,.webp"
                           className={styles.file}
                           onChange={handleImage}
@@ -165,7 +198,7 @@ const EditUserInfor = ({
                       )}
                     />
                     <label htmlFor="" className="text-slate-500 text-xs">
-                      {}
+                      {formik.values?.bio?.length}
                       /80
                     </label>
                   </div>
@@ -177,8 +210,8 @@ const EditUserInfor = ({
                       initialValues.fullname !== formik.values.fullname ||
                       initialValues.bio !== formik.values.bio ||
                       changeImage === true
-                        ? 'bg-[#fe2c55] text-white'
-                        : 'bg-gray-200 text-slate-500 pointer-events-none'
+                        ? "bg-[#fe2c55] text-white"
+                        : "bg-gray-200 text-slate-500 pointer-events-none"
                     } `}
                     type="submit"
                   >
@@ -192,7 +225,6 @@ const EditUserInfor = ({
                   </button>
                 </div>
               </div>
-              <ToastContainer />
             </form>
           );
         }}
