78ed04140c2a214b9c0c2bbb7c65113e6e386faf
trungdq1 <trungdq1@smartosc.com>
2023-06-30 09:41:05 +0700

    new firebase

--- tiktok-app/src/firebase/index.ts
+++ tiktok-app/src/firebase/index.ts
@@ -5,17 +5,27 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAB4NtMPHCA4uNJ0fY6D8V82LIW3L6yw6k",
+//     authDomain: "mocktiktok-f444b.firebaseapp.com",
+//     projectId: "mocktiktok-f444b",
+//     storageBucket: "mocktiktok-f444b.appspot.com",
+//     messagingSenderId: "427042367630",
+//     appId: "1:427042367630:web:bbc88d28e845f3eb8201df",
+//     measurementId: "G-2RG6MQFMZK"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAB4NtMPHCA4uNJ0fY6D8V82LIW3L6yw6k",
-    authDomain: "mocktiktok-f444b.firebaseapp.com",
-    projectId: "mocktiktok-f444b",
-    storageBucket: "mocktiktok-f444b.appspot.com",
-    messagingSenderId: "427042367630",
-    appId: "1:427042367630:web:bbc88d28e845f3eb8201df",
-    measurementId: "G-2RG6MQFMZK"
+    apiKey: "AIzaSyB0HWtEpZarGTWRs-oCNmiLOf5cp7EKzk4",
+    authDomain: "tiktokbackup-ea31b.firebaseapp.com",
+    projectId: "tiktokbackup-ea31b",
+    storageBucket: "tiktokbackup-ea31b.appspot.com",
+    messagingSenderId: "513146849769",
+    appId: "1:513146849769:web:3e52b267fb424c1a19e378",
+    measurementId: "G-RRMWRQE2MR"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
